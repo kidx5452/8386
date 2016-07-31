2db7d609954cdf58673c7e2c2f138dfcbd940b91
Minh Dat <le.minh.dat.humg@gmail.com>
2016-08-01 01:26:52 +0700

    update

--- webapp/app/config/config.php
+++ webapp/app/config/config.php
@@ -1,23 +1,25 @@
 <?php
-
-defined('APP_PATH') || define('APP_PATH', realpath('.'));
-
-return new \Phalcon\Config(array(
-    'database' => array(
-        'adapter'     => 'Mysql',
-        'host'        => 'localhost',
-        'username'    => 'root',
-        'password'    => '123456$',
-        'dbname'      => 'thanglonga_demo',
-        'charset'     => 'utf8',
-    ),
-    'application' => array(
-        'coreDir' => APP_PATH . '/app/core/',
-        'modulesDir' => APP_PATH . '/app/modules/',
-        'modelsDir' => APP_PATH . '/app/core/models/',
-        'libraryDir'     => APP_PATH . '/app/library/',
-        'cacheDir'       => APP_PATH . '/app/cache/',
-        'baseUri'        => '/',
-    ),
-    'modules'=>array("index","article","page")
-));
+    defined('APP_PATH') || define('APP_PATH', realpath('.'));
+    return new \Phalcon\Config([
+        'database' => [
+            'adapter' => 'Mysql',
+            'host' => 'localhost',
+            'username' => 'root',
+            'password' => '123456$',
+            'dbname' => 'thanglonga_demo',
+            'charset' => 'utf8',
+        ],
+        'application' => [
+            'coreDir' => APP_PATH . '/app/core/',
+            'modulesDir' => APP_PATH . '/app/modules/',
+            'modelsDir' => APP_PATH . '/app/core/models/',
+            'libraryDir' => APP_PATH . '/app/library/',
+            'cacheDir' => APP_PATH . '/app/cache/',
+            'baseUri' => '/',
+        ],
+        'modules' => [
+            "index",
+            "article",
+            "page"
+        ]
+    ]);
